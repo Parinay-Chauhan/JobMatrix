b8aba7e5336cbf8488d235ff455213f6f3134db2
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-16 13:42:21 +0530

    feat: initialize client application with React and Vite
    - Add SVG icons for various services (Bluesky, Discord, Documentation, GitHub, Social, X).
    - Create main application component (App.tsx).
    - Add hero image and React logo SVG.
    - Set up Vite configuration for React.
    - Establish TypeScript configuration for application and node environments.
    - Implement entry point for React application (main.tsx).

---
+++ Backend/test.js
@@ -0,0 +1,19 @@
+// TODO: recruitermodel
+
+// TOdo: usercontroller me role add karna
+// TODO: recruitercontroller
+//  createRecruiterProfile, getRecruiterProfile, updateRecruiterProfile
+//  updateCompanyLogo, deleteCompanyLogo
+// TODO: recruiter routes
+
+// TODO: job model, job controller, job routes
+
+
+// TODO: application model, application controller, application routes
+
+
+// TODO: job controller me getAllJobs ko update kiya hai, recruiter ke liye filter karne ke liye, or ek toggleJobStatus controller banaya hai, iski routes nh likhe hai
+
+// TODO: recuriter controller me getRecruiterDashboardStats banaya hai or isme job and application import kiya , iski routes nh likhe hai, 
+
+// TODO: notification model, notification controller me applyjob me notification create kiya hai, iski routes nh likhe hai,
