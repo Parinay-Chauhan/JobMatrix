db3326e0201bcc153ad68b44aa80c0062f3d7123
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-18 00:09:35 +0530

    feat: integrate useNotificationSocket hook into App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { AppRoutes } from "./routes/AppRoutes";
+import { useNotificationSocket } from "./hooks/useNotificationSocket";
 
 export const App: React.FC = () => {
+  useNotificationSocket();
+
   return (
     <BrowserRouter>
       <AuthProvider>
