efe57ad27b33c91b32fe8578a4e620ece7c7edca
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-20 00:46:40 +0530

    refactor: remove AuthProvider from App component for simplified routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
 import { AppRoutes } from "./routes/AppRoutes";
 import { useNotificationSocket } from "./hooks/useNotificationSocket";
+
 
 export const App: React.FC = () => {
   useNotificationSocket();
 
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
+      <AppRoutes />
     </BrowserRouter>
   );
 };
+
 
 export default App;
