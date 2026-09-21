673a7615a3e14cdca9584beb2467ac243b8eb27c
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-21 16:10:01 +0530

    feat(queries): add index file to export hooks for jobs, applications, candidates, recruiters, and notifications

---
+++ client/src/hooks/queries/index.ts
@@ -0,0 +1,5 @@
+export * from "./useJobsQuery";
+export * from "./useApplicationsQuery";
+export * from "./useCandidateQuery";
+export * from "./useRecruiterQuery";
+export * from "./useNotificationsQuery";
