8e2b1873d9bf9a122ad2f1ef5991b21cef37584d
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-09-21 16:18:00 +0530

    feat(app): add Toaster component for enhanced notification display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
 import { AppRoutes } from "./routes/AppRoutes";
 import { useNotificationSocket } from "./hooks/useNotificationSocket";
-
 
 export const App: React.FC = () => {
   useNotificationSocket();
 
   return (
     <BrowserRouter>
+      <Toaster richColors position="top-right" closeButton />
       <AppRoutes />
     </BrowserRouter>
   );
 };
-
 
 export default App;
